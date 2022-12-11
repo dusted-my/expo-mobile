8b74b38b18f3539ec075182f315b3d5a16f4b3c5
Elyna Fock <elinayen2000@gmail.com>
2022-12-11 11:59:48 +0800

    splash screen

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Badge } from 'react-native-paper';
+import * as React from 'react';
+import { List, MD3Colors } from 'react-native-paper';
+
 
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Badge>2</Badge>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image style={{resizeMode : "contain",height: 100,width: 200}} source={require('./assets/dusted.png') }/>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
