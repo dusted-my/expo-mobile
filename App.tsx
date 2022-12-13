b71fa402cb1453c02a117c54526dbe19ce4834df
Elyna Fock <elinayen2000@gmail.com>
2022-12-13 22:29:50 +0800

    Font

--- App.tsx
+++ App.tsx
@@ -1,36 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Badge } from 'react-native-paper';
-import * as React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-const Stack = createNativeStackNavigator();
+import { Provider as PaperProvider } from "react-native-paper";
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <PaperProvider>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
       </PaperProvider>
     </NavigationContainer>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,31 +1,51 @@
+import {
+  Inter_500Medium,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import { useFonts } from "expo-font";
 import React from "react";
 import { Image, View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 
-const HomeScreen = ({ navigation }) => (
-  <View style={styles.body}>
-    <Image style={styles.logo} source={require("../assets/dusted.png")} />
-    <Text style={styles.text}>We made cleaning service easier for you</Text>
-    <View style={styles.container}>
-      <Button
-        style={styles.button}
-        labelStyle={styles.buttonLabel}
-        mode="contained"
-        onPress={() => navigation.navigate("Login")}
-      >
-        Log In
-      </Button>
-      <Button
-        style={styles.button}
-        labelStyle={styles.buttonLabel}
-        mode="contained"
-        onPress={() => navigation.navigate("Login")}
-      >
-        Register
-      </Button>
+const HomeScreen = ({ navigation }) => {
+  let [fontsLoaded] = useFonts({
+    Inter_700Bold,
+    Inter_900Black,
+    Inter_500Medium,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.body}>
+      <Image style={styles.logo} source={require("../assets/dusted.png")} />
+      <Text style={styles.text}>We made cleaning service easier for you</Text>
+      <View style={styles.container}>
+        <Button
+          style={styles.button}
+          labelStyle={styles.buttonLabel}
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Log In
+        </Button>
+        <Button
+          style={styles.button}
+          labelStyle={styles.buttonLabel}
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Register
+        </Button>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   body: {
@@ -42,9 +62,10 @@ const styles = StyleSheet.create({
   },
   text: {
     paddingVertical: 64,
-    fontSize: 40,
+    fontSize: 42,
     fontWeight: "bold",
     textAlign: "center",
+    fontFamily: "Inter_700Bold",
   },
   container: {
     width: "100%",
@@ -57,6 +78,7 @@ const styles = StyleSheet.create({
   buttonLabel: {
     fontSize: 24,
     lineHeight: 32,
+    fontFamily: "Inter_500Medium",
   },
 });
 
