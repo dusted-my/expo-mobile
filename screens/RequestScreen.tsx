4b422f85cedae0c8feb7dd2b85e2806c7dd3b581
Elyna Fock <elinayen2000@gmail.com>
2022-12-19 21:29:53 +0800

    Request become cleaner (8/10)

--- screens/RequestScreen.tsx
+++ screens/RequestScreen.tsx
@@ -5,26 +5,56 @@ import {
 } from "@expo-google-fonts/inter";
 import { useFonts } from "expo-font";
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { Button, RadioButton, TextInput } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
 
 const RequestScreen = ({ navigation }) => {
   const [nric, setNric] = useState("");
   const [dob, setDob] = useState("");
+  const [value, setValue] = React.useState("first");
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+  const [nricFront, setNricFront] = useState(null);
+  const [nricBack, setNricBack] = useState(null);
+
   let [fontsLoaded] = useFonts({
     Inter_700Bold,
     Inter_500Medium,
-    Inter_600SemiBold,
   });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const pickImage = async (type: "front" | "back") => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (result.canceled) return;
+
+    switch (type) {
+      case "front":
+        setNricFront(result.assets[0].uri);
+        break;
+      case "back":
+        setNricBack(result.assets[0].uri);
+        break;
+    }
+  };
 
   function handleSubmit() {
     // TODO: INTEGRATE LOG IN
     navigation.navigate("Home");
   }
   return (
-    <View>
+    <ScrollView>
       <View style={styles.container}>
         <Text style={styles.title}>Application to Become Cleaner</Text>
         <View style={styles.form}>
@@ -60,19 +90,63 @@ const RequestScreen = ({ navigation }) => {
             onChangeText={(text) => setAddress(text)}
             mode="outlined"
           />
+          <Text style={styles.textGender}>Gender:</Text>
+          <View style={styles.radioButton}>
+            <RadioButton.Group
+              onValueChange={(newValue) => setValue(newValue)}
+              value={value}
+            >
+              <View style={styles.radioButton}>
+                <RadioButton.Android value="first" />
+                <Text style={styles.radioButtonText}>Male</Text>
+              </View>
+              <View style={styles.radioButton}>
+                <RadioButton.Android value="second" />
+                <Text style={styles.radioButtonText}>Female</Text>
+              </View>
+            </RadioButton.Group>
+          </View>
+          <Text style={styles.textGender}>Photocopy of NRIC: </Text>
 
-          <Button
-            style={styles.button}
-            labelStyle={styles.buttonLabel}
-            buttonColor="#000000"
-            mode="contained"
-            onPress={handleSubmit}
-          >
-            Submit
-          </Button>
+          <View style={styles.photo}>
+            <View style={styles.rectangleBorder}>
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <Button onPress={() => pickImage("front")}>NRIC Front</Button>
+                {nricFront && (
+                  <Image
+                    source={{ uri: nricFront }}
+                    style={{ width: 200, height: 200 }}
+                  />
+                )}
+              </View>
+            </View>
+          </View>
+          <View style={styles.rectangleBorder}>
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Button onPress={() => pickImage("back")}>NRIC Back</Button>
+              {nricBack && (
+                <Image
+                  source={{ uri: nricBack }}
+                  style={{ width: 200, height: 200 }}
+                />
+              )}
+            </View>
+          </View>
+
+          <View style={styles.buttomPadding}>
+            <Button
+              style={styles.button}
+              labelStyle={styles.buttonLabel}
+              buttonColor="#000000"
+              mode="contained"
+              onPress={handleSubmit}
+            >
+              Submit
+            </Button>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -88,6 +162,41 @@ const styles = StyleSheet.create({
   form: {
     width: "100%",
     paddingVertical: 24,
+  },
+  textGender: {
+    fontSize: 18,
+    fontFamily: "Inter_500Medium",
+    padding: 10,
+  },
+  radioButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 30,
+  },
+  radioButtonText: {
+    fontSize: 16,
+    fontFamily: "Inter_500Medium",
+  },
+  photo: {
+    paddingBottom: 30,
+  },
+  rectangleBorder: {
+    borderColor: "#DDD",
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent: "center",
+    paddingVertical: 60,
+    paddingBottom: 30,
+    textAlign: "center",
+  },
+  photocopy: {
+    height: 128,
+    width: 128,
+    borderColor: "salmon",
+    position: "absolute",
+    zIndex: 99,
+    top: "100%",
+    left: "100%",
   },
   button: {
     marginVertical: 12,
@@ -109,6 +218,9 @@ const styles = StyleSheet.create({
   inputOutline: {
     borderRadius: 100,
     borderColor: "#AAA",
+  },
+  buttomPadding: {
+    paddingTop: 30,
   },
 });
 export default RequestScreen;
