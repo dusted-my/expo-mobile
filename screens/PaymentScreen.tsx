b447d60c28ccab4980afab76106b23b428f7badf
Elyna Fock <elinayen2000@gmail.com>
2022-12-30 13:24:13 +0800

    Payment Screen (10/10)

--- screens/PaymentScreen.tsx
+++ screens/PaymentScreen.tsx
@@ -1,10 +1,18 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { Button } from "react-native-paper";
 
 const options = ["Credit/Debit", "Cash", "E-wallet"];
 
-const PaymentScreen = () => {
+const PaymentScreen = ({ navigation }) => {
   const [selected, setSelected] = useState("");
 
   function select(value: string) {
@@ -12,79 +20,87 @@ const PaymentScreen = () => {
   }
 
   return (
-    <View style={styles.body}>
-      <Image
-        style={styles.logo}
-        source={require("../assets/dusted.png")}
-      ></Image>
-      <View style={styles.card}>
-        <Text style={styles.page}>Booking Details</Text>
+    <ScrollView>
+      <View style={styles.body}>
         <Image
-          style={styles.profile}
-          source={require("../assets/cleaner.jpg")}
-        />
-        <Text style={styles.name}>Anna Jowel</Text>
-        <Text>Fair:</Text>
-        <Text style={styles.price}>RM 80</Text>
-        <View>
-          <View style={styles.buttonRow}>
-            {options.map((option) => (
-              <Button
-                key={option}
-                style={[
-                  styles.complaintButton,
-                  selected === option && styles.selectedButton,
-                ]}
-                labelStyle={[
-                  styles.complaintButtonLabel,
-                  selected === option && styles.selectedButtonLabel,
-                ]}
-                mode="outlined"
-                onPress={() => select(option)}
-              >
-                {option}
-              </Button>
-            ))}
-          </View>
-          <View style={styles.information}>
-            <View style={styles.details}>
-              <Text>Location: </Text>
-              <Text style={{ flexWrap: "wrap", alignItems: "flex-start" }}>
+          style={styles.logo}
+          source={require("../assets/dusted.png")}
+        ></Image>
+        <View style={styles.card}>
+          <Text style={styles.page}>Booking Details</Text>
+          <Image
+            style={styles.profile}
+            source={require("../assets/cleaner.jpg")}
+          />
+          <Text style={styles.name}>Anna Jowel</Text>
+          <Text>Fair:</Text>
+          <Text style={styles.price}>RM 80</Text>
+          <View>
+            <View style={styles.buttonRow}>
+              {options.map((option) => (
+                <Button
+                  key={option}
+                  style={[
+                    styles.complaintButton,
+                    selected === option && styles.selectedButton,
+                  ]}
+                  labelStyle={[
+                    styles.complaintButtonLabel,
+                    selected === option && styles.selectedButtonLabel,
+                  ]}
+                  mode="outlined"
+                  onPress={() => select(option)}
+                >
+                  {option}
+                </Button>
+              ))}
+            </View>
+            <View>
+              <Text style={styles.title}>Location: </Text>
+              <Text style={styles.details}>
                 Jalan 29 Pandan Indah Wilayah Persekutuan 56200 Malaysia
               </Text>
             </View>
-            <View style={styles.details}>
-              <Text>Contact No.: </Text>
-              <Text style={{ flexWrap: "wrap", alignItems: "flex-start" }}>
-                03-9274-3421
-              </Text>
+            <View>
+              <Text style={styles.title}>Contact No.: </Text>
+              <Text style={styles.details}>03-9274-3421</Text>
             </View>
-            <View style={styles.details}>
-              <Text>Date: </Text>
-              <Text style={{ flexWrap: "wrap", alignItems: "flex-start" }}>
-                14 Nov 2022 14:30 - 15:30
-              </Text>
+            <View>
+              <Text style={styles.title}>Date: </Text>
+              <Text style={styles.details}>14 Nov 2022 14:30 - 15:30</Text>
             </View>
+
+            <TouchableOpacity onPress={() => navigation.navigate("Payment")}>
+              <LinearGradient
+                colors={["#CF91FF", "#5782F5"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0.9, y: 0.5 }}
+                style={[styles.button, styles.buttonBook]}
+              >
+                <Text style={styles.buttonLabelBook}>Pay</Text>
+              </LinearGradient>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    // marginTop: 40,
-    justifyContent: "space-between",
-    height: "100%",
-    padding: 32,
-    paddingBottom: 50,
-  },
   body: {
     alignItems: "center",
+    margin: 20,
+    borderWidth: 1,
+    borderColor: "#FFF",
+    shadowColor: "000",
+    borderRadius: 10,
+    backgroundColor: "#FFF",
+    paddingHorizontal: 50,
+    paddingVertical: 20,
   },
   logo: {
-    marginTop: "5%",
+    // marginTop: "10%",
     resizeMode: "contain",
     height: 42.72,
     width: 181,
@@ -108,11 +124,11 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     fontWeight: "400",
-    paddingBottom: 20,
+    paddingBottom: 16,
   },
   price: {
     fontSize: 24,
-    paddingVertical: 20,
+    paddingVertical: 18,
     fontWeight: "500",
   },
   buttonRow: {
@@ -136,9 +152,35 @@ const styles = StyleSheet.create({
   information: {
     alignItems: "center",
   },
+
+  title: {
+    fontSize: 18,
+    fontWeight: "400",
+
+    paddingTop: 20,
+  },
   details: {
-    // flexDirection: "row",
-    maxWidth: "50%",
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+
+    fontSize: 16,
+    fontWeight: "300",
+    // textAlign: "center",
+  },
+  button: {
+    marginTop: 40,
+    borderRadius: 100,
+    borderWidth: 0,
+  },
+  buttonBook: {
+    paddingVertical: 16,
+  },
+  buttonLabelBook: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontFamily: "Inter_500Medium",
+    textAlign: "center",
+    color: "#FFFFFF",
   },
 });
 
